8d10fe2e271d0b6ece0c0f6347db5da18d779455
Yurij Izhevskij <yuriji@recommendlabs.com>
2018-03-04 18:07:00 +0200

    Исправлены имена полей формы  и показан проимер в контроллере вывода всех данных _POST

--- app/code/local/RonisBT/Banners/Block/Adminhtml/Banners/Edit/Form.php
+++ app/code/local/RonisBT/Banners/Block/Adminhtml/Banners/Edit/Form.php
@@ -32,14 +32,16 @@ class RonisBT_Banners_Block_Adminhtml_Banners_Edit_Form extends Mage_Adminhtml_B
 
         $form = new Varien_Data_Form(
             array(
-                'id' => 'edit_form',
-                'action' => $this->getData('action'), 
-                'method' => 'post')
+                "id" => "edit_form",
+                "action" => $this->getUrl("*/*/save", array("banner_id" => $this->getRequest()->getParam("banner_id"))),
+                "method" => "post",
+                "enctype" =>"multipart/form-data",
+                )
         );
 
         $form->setHtmlIdPrefix('block_');
 
-        $fieldset = $form->addFieldset('base_fieldset', array('legend'=>Mage::helper('banners')->__('General Information'), 'class' => 'fieldset-wide'));
+        $fieldset = $form->addFieldset('base_fieldset', array('legend'=>Mage::helper('banners')->__('Item Information'), 'class' => 'fieldset-wide'));
 
         if ($model->getBlockId()) {
             $fieldset->addField('block_id', 'hidden', array(
@@ -49,40 +51,40 @@ class RonisBT_Banners_Block_Adminhtml_Banners_Edit_Form extends Mage_Adminhtml_B
 
         $fieldset->addField('title', 'text', array(
             'name'      => 'title',
-            'label'     => Mage::helper('banners')->__('Block Title'),
-            'title'     => Mage::helper('banners')->__('Block Title'),
+            'label'     => Mage::helper('banners')->__('Title'),
+            'title'     => Mage::helper('banners')->__('Banner title'),
             'required'  => true,
         ));
         
         $fieldset->addField('url', 'text', array(
-            'name'      => 'title',
+            'name'      => 'url',
             'label'     => Mage::helper('banners')->__('Link'),
-            'title'     => Mage::helper('banners')->__('Block Title'),
+            'title'     => Mage::helper('banners')->__('Banner link'),
             'required'  => true,
         ));
         
         $fieldset->addField('image', 'text', array(
-            'name'      => 'title',
-            'label'     => Mage::helper('banners')->__('Link'),
-            'title'     => Mage::helper('banners')->__('Block Title'),
+            'name'      => 'image',
+            'label'     => Mage::helper('banners')->__('Image'),
+            'title'     => Mage::helper('banners')->__('Banner image'),
             'required'  => true,
         ));
         
         $fieldset->addField('position_sort', 'text', array(
-            'name'      => 'title',
-            'label'     => Mage::helper('banners')->__('Link'),
-            'title'     => Mage::helper('banners')->__('Block Title'),
+            'name'      => 'position_sort',
+            'label'     => Mage::helper('banners')->__('Position sort'),
+            'title'     => Mage::helper('banners')->__('Position sort'),
             'required'  => true,
         ));
         
         
         $fieldset->addField('block_status', 'select', array(
             'label'     => Mage::helper('banners')->__('Status'),
-            'title'     => Mage::helper('banners')->__('Status'),
+            'title'     => Mage::helper('banners')->__('Banner status'),
             'name'      => 'block_status',
             'index'     => "block_status",
             'required'  => true,
-            'options'   => Mage::getModel('banners/source_status')->toOptionArray(),
+            'options'   => Mage::getModel('banners/source_status')->toArray(),
         ));
         
        
--- app/code/local/RonisBT/Banners/controllers/Adminhtml/RonisbannersController.php
+++ app/code/local/RonisBT/Banners/controllers/Adminhtml/RonisbannersController.php
@@ -20,13 +20,24 @@ class RonisBT_Banners_Adminhtml_RonisbannersController extends Mage_Adminhtml_Co
     
     public function editAction()
     {
-        $id = $this->getRequest()->getParam('block_id');
+        $id = $this->getRequest()->getParam('banner_id');
         
         Mage::register('banners_block', Mage::getModel('banners/bannereditor')->load($id));
           
         $this->loadLayout();
         $this->_addContent($this->getLayout()->createBlock('banners/adminhtml_banners_edit'));
         $this->renderLayout();
+        
+    }
+    
+    public function saveAction()
+    {
+        $id = $this->getRequest()->getParam('banner_id');
+        $banner = Mage::getModel('banners/bannereditor')->load($id);
+        
+        $banner
+                ->setData($this->getRequest()->getParams())
+                ->save();
         
     }
 
