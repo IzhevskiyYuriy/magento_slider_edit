99392d2fb1173c9ad2108734eba2d7c9324bde05
Yurij Izhevskij <yuriji@recommendlabs.com>
2018-03-06 21:48:30 +0200

    класс подготавливает контент для frontenda и передает данные по возрастанию

---
+++ app/code/local/RonisBT/Banners/Block/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+class RonisBT_Banners_Block_Index extends Mage_Core_Block_Template
+{
+
+    public function getBannersContent()
+    {
+       return Mage::getResourceModel('banners/bannereditor_collection')
+           ->addFieldToFilter('block_status', ['eq' => RonisBT_Banners_Model_Source_Status::STATUS_ENABLED])
+           ->setOrder('position_sort', 'ASC');
+    }
+
+    public function getImageSrc()
+    {
+        return Mage::getBaseUrl('media'). $this->getImage();
+    }
+}
