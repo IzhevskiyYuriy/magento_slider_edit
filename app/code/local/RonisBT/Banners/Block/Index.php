54018ec908c895837e6b96cae4b5d7157ee142f7
Yurij Izhevskij <yuriji@recommendlabs.com>
2018-03-06 22:40:30 +0200

    подключен слайдер, и реализован его вывод на тестовую страницу

--- app/code/local/RonisBT/Banners/Block/Index.php
+++ app/code/local/RonisBT/Banners/Block/Index.php
@@ -2,7 +2,6 @@
 
 class RonisBT_Banners_Block_Index extends Mage_Core_Block_Template
 {
-
     public function getBannersContent()
     {
        return Mage::getResourceModel('banners/bannereditor_collection')
