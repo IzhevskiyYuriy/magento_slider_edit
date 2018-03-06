5225cf7456c0ab5abca394be3996d7e290f15b71
Yurij Izhevskij <yuriji@recommendlabs.com>
2018-03-06 22:21:30 +0200

    Добавлен Renderer - отвечает за отображение картинки на странице редактирования

--- app/code/local/RonisBT/Banners/Block/Adminhtml/Banners/Edit/Form.php
+++ app/code/local/RonisBT/Banners/Block/Adminhtml/Banners/Edit/Form.php
@@ -27,57 +27,53 @@ class RonisBT_Banners_Block_Adminhtml_Banners_Edit_Form extends Mage_Adminhtml_B
 */
     protected function _prepareForm()
     {
-        
-        $model = Mage::registry('banners_block');
 
+        $model = Mage::registry('banners_block');
         $form = new Varien_Data_Form(
             array(
                 "id" => "edit_form",
                 "action" => $this->getUrl("*/*/save", array("banner_id" => $this->getRequest()->getParam("banner_id"))),
                 "method" => "post",
                 "enctype" =>"multipart/form-data",
-                )
+            )
         );
-
         $form->setHtmlIdPrefix('block_');
-
         $fieldset = $form->addFieldset('base_fieldset', array('legend'=>Mage::helper('banners')->__('Item Information'), 'class' => 'fieldset-wide'));
-
         if ($model->getBlockId()) {
             $fieldset->addField('block_id', 'hidden', array(
                 'name' => 'block_id',
             ));
         }
-
         $fieldset->addField('title', 'text', array(
             'name'      => 'title',
             'label'     => Mage::helper('banners')->__('Title'),
             'title'     => Mage::helper('banners')->__('Banner title'),
             'required'  => true,
         ));
-        
+
         $fieldset->addField('url', 'text', array(
             'name'      => 'url',
             'label'     => Mage::helper('banners')->__('Link'),
             'title'     => Mage::helper('banners')->__('Banner link'),
             'required'  => true,
         ));
-        
-        $fieldset->addField('image', 'file', array(
+
+        $fieldset->addType('image', 'RonisBT_Banners_Block_Adminhtml_Banners_Edit_Renderer_Imagebanners');
+        $fieldset->addField('image', 'image', array(
             'name'      => 'image',
             'label'     => Mage::helper('banners')->__('Image'),
             'title'     => Mage::helper('banners')->__('Banner image'),
             'required'  => true,
         ));
-        
+
         $fieldset->addField('position_sort', 'text', array(
             'name'      => 'position_sort',
             'label'     => Mage::helper('banners')->__('Position sort'),
             'title'     => Mage::helper('banners')->__('Position sort'),
             'required'  => true,
         ));
-        
-        
+
+
         $fieldset->addField('block_status', 'select', array(
             'label'     => Mage::helper('banners')->__('Status'),
             'title'     => Mage::helper('banners')->__('Banner status'),
@@ -86,12 +82,15 @@ class RonisBT_Banners_Block_Adminhtml_Banners_Edit_Form extends Mage_Adminhtml_B
             'required'  => true,
             'options'   => Mage::getModel('banners/source_status')->toArray(),
         ));
-        
-       
+
+        if (!$model->getId()) {
+            $model->setData('block_status', '1');
+        }
+
+
         $form->setValues($model->getData());
         $form->setUseContainer(true);
         $this->setForm($form);
-
         return parent::_prepareForm();
     }
 
